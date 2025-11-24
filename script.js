6f7be47fc53830ed943d87dc3a07a281968c44fc
ceeriss <komarchuk.nikkom@mail.ru>
2025-11-24 14:57:18 +0300

    dz-done2

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const users = [
   { id: 1, name: "Вася" },
 ];
 
-// Используем Map для хранения объектов по уникальному ключу (id)
-const uniqueUsers = Array.from(
-  new Map(users.map(user => [user.id, user])).values()
-);
+// Используем Set для получения уникальных id
+const uniqueIds = [...new Set(users.map(user => user.id))];
+
+// Используем find для нахождения первого объекта с каждым id
+const uniqueUsers = uniqueIds.map(id => users.find(user => user.id === id));
 
 console.log(uniqueUsers);
 // [{ id: 1, name: "Вася" }, { id: 2, name: "Петя" }]
