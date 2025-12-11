19a327c298dbda39934a62fc87b4d0a0e67ec429
ceeriss <komarchuk.nikkom@mail.ru>
2025-12-11 09:56:20 +0300

    done again

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-const Persona = function (rasa, name, language,) {
-	this.rasa = rasa;
-	this.name = name;
-	this.language = language;
+// 1. Базовый класс Персонажа
+const Persona = function(rasa, name, language) {
+    this.rasa = rasa;
+    this.name = name;
+    this.language = language;
 }
 
-Persona.prototype.talk = function () {
-	return `Мое имя: ${this.name} и язык: ${this.language}`
+// Метод говорить - ВЫВОДИТ в консоль, а не возвращает
+Persona.prototype.talk = function() {
+    console.log(`Мое имя: ${this.name} и язык: ${this.language}`);
 }
-const Ork = new Persona('Орк', 'Данила', 'Оркский')
 
-Ork.weapon = 'Секира'
-
-Ork.attack = function () {
-	return console.log('Удар!!!');
-	
+// 2. Класс Орка (наследуется от Persona)
+const Ork = function(name, language, weapon) {
+    // Вызываем конструктор родителя
+    Persona.call(this, 'Орк', name, language);
+    this.weapon = weapon;
 }
-console.log(Ork);
-Ork.attack()
 
+// Настраиваем прототипное наследование
+Ork.prototype = Object.create(Persona.prototype);
+Ork.prototype.constructor = Ork;
 
-const Elf = new Persona('Эльф', 'Валерия', 'Эльфийский')
-
-Elf.magic = 'Заклинание любви'
-
-Elf.createSpell = function () {
-	return console.log('Я создала заклинание');
-	
-	
+// Добавляем метод удара в прототип Орка
+Ork.prototype.attack = function() {
+    console.log(`${this.name} бьет ${this.weapon}ой! Удар!!!`);
 }
-console.log(Elf);
-Elf.createSpell()
+
+// 3. Класс Эльфа (наследуется от Persona)
+const Elf = function(name, language, magic) {
+    // Вызываем конструктор родителя
+    Persona.call(this, 'Эльф', name, language);
+    this.magic = magic;
+}
+
+// Настраиваем прототипное наследование
+Elf.prototype = Object.create(Persona.prototype);
+Elf.prototype.constructor = Elf;
+
+// Добавляем метод создания заклинания в прототип Эльфа
+Elf.prototype.createSpell = function() {
+    console.log(`${this.name} создала заклинание: "${this.magic}"`);
+}
+
+// 4. ТЕПЕРЬ создаем экземпляры классов
+const ork = new Ork('Данила', 'Оркский', 'Секира');
+const elf = new Elf('Валерия', 'Эльфийский', 'Заклинание любви');
+
+console.log('=== Орк ===');
+ork.talk();      // Метод унаследован от Persona
+ork.attack();    // Собственный метод Орка
+console.log(ork); // Показывает все свойства
+
+console.log('\n=== Эльф ===');
+elf.talk();          // Метод унаследован от Persona
+elf.createSpell();   // Собственный метод Эльфа
+console.log(elf);    // Показывает все свойства
